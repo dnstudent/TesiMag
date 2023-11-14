c99a52edc4870e853e22ce96179a02670317e5c0
dnstudent <davide.nicoli1@studenti.unimi.it>
2023-11-14 21:34:30 +0100

    Working on a global cluster classifier, improving plotting and pairing utilities

--- src/plot/plot_helpers.R
+++ src/plot/plot_helpers.R
@@ -18,15 +18,38 @@ plot_series <- function(tvar, s1, s2, ...) {
         theme(plot.title = element_text(size = rel(0.8), color = "#ff5900"), plot.subtitle = element_text(size = rel(0.8), color = "#40d8e0"))
 }
 
-plot.sciavsdpc <- function(tvar, id.scia, id.dpc, flavor.dpc) {
+plot.sciavsdpc <- function(tvar, id.scia, id.dpc, flavor.dpc, diffs = TRUE, start_date = NULL, anagrafica.scia = NULL) {
     s.scia <- read.series.single("SCIA", tvar, id.scia) |>
         drop_na({{ tvar }}) |>
-        fill_gaps()
+        fill_gaps() |>
+        rename(T = {{ tvar }})
     s.dpc <- read.series.single("DPC", tvar, id.dpc, flavor = flavor.dpc) |>
         drop_na({{ tvar }}) |>
-        fill_gaps()
-    data <- list(s.scia, s.dpc)
-    data <- bind_rows(list(s.scia |> as_tibble(), s.dpc |> as_tibble()) |> setNames(c(id.scia, id.dpc)), .id = "station")
-    ggplot(data) +
-        geom_line(aes(date, .data[[tvar]], color = station, linetype = station))
+        fill_gaps() |>
+        rename(T = {{ tvar }})
+    if (!is.null(start_date)) {
+        s.scia <- filter(s.scia, date >= as.Date(start_date))
+        s.dpc <- filter(s.dpc, date >= as.Date(start_date))
+    }
+    if (diffs) {
+        inner_join(s.scia, s.dpc, by = "date") |>
+            mutate(
+                diffs = T.x - T.y,
+                nas = case_when(
+                    is.na(T.x) & is.na(T.y) ~ "both",
+                    is.na(T.x) ~ "scia",
+                    is.na(T.y) ~ "dpc",
+                    .default = NA
+                ) |> factor(levels = c("none", "scia", "dpc", "both"))
+            ) |>
+            ggplot() +
+            geom_rect(aes(NULL, NULL, xmin = date, xmax = lag(date), fill = nas), ymin = -0.5, ymax = 0.5, alpha = 0.9, na.rm = TRUE) +
+            geom_line(aes(date, diffs), na.rm = TRUE) +
+            labs(title = str_glue("{id.scia}({anagrafica.scia}) vs {id.dpc}"))
+    } else {
+        data <- list(s.scia, s.dpc)
+        data <- bind_rows(list(s.scia |> as_tibble(), s.dpc |> as_tibble()) |> setNames(c(id.scia, id.dpc)), .id = "station")
+        ggplot(data) +
+            geom_line(aes(date, T, color = station, linetype = station))
+    }
 }
