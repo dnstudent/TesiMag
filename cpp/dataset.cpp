8113b47400fa8a9ed32c3ca2839c1b026f800fa0
dnstudent <davide.nicoli1@studenti.unimi.it>
2023-11-14 09:43:33 +0100

    Working on dataset

--- cpp/dataset.cpp
+++ cpp/dataset.cpp
@@ -7,11 +7,11 @@
 #include <memory>
 #include <iostream>
 #include <filesystem>
-
-#define TMIN_PATH fs::path("/Users/davidenicoli/Local_Workspace/Datasets/SCIA/giornaliere/minime/db/series.parquet")
-#define TMAX_PATH fs::path("/Users/davidenicoli/Local_Workspace/Datasets/SCIA/giornaliere/massime/db/series.parquet")
-
 namespace fs = std::filesystem;
+
+#define SCIA_PATH fs::path("/Users/davidenicoli/Local_Workspace/Datasets/SCIA")
+#define TMIN_PATH fs::path(SCIA_PATH / "giornaliere/minime/db/series.parquet")
+#define TMAX_PATH fs::path(SCIA_PATH / "giornaliere/massime/db/series.parquet")
 
 arrow::Result<std::shared_ptr<arrow::Table>> ReadFile(const fs::path &path_to_file)
 {
@@ -32,9 +32,29 @@ arrow::Result<std::shared_ptr<arrow::Table>> ReadFile(const fs::path &path_to_fi
 
 arrow::Status RunMain()
 {
-    ARROW_ASSIGN_OR_RAISE(auto tmin, ReadFile(TMIN_PATH));
-    ARROW_ASSIGN_OR_RAISE(auto tmax, ReadFile(TMAX_PATH));
-    std::cout << tmin->Slice(0, 10)->ToString() << std::endl;
+    ARROW_ASSIGN_OR_RAISE(const auto lfs, arrow::fs::FileSystemFromUriOrPath(SCIA_PATH));
+    arrow::fs::FileSelector file_selector;
+    file_selector.base_dir = SCIA_PATH;
+    arrow::dataset::FileSystemFactoryOptions options;
+    options.partitioning = arrow::dataset::HivePartitioning::MakeFactory();
+    const auto read_format = std::make_shared<arrow::dataset::ParquetFileFormat>();
+    ARROW_ASSIGN_OR_RAISE(const auto factory, arrow::dataset::FileSystemDatasetFactory::Make(lfs, {TMIN_PATH, TMAX_PATH}, read_format, options));
+    ARROW_ASSIGN_OR_RAISE(const auto dataset, factory->Finish());
+    ARROW_ASSIGN_OR_RAISE(auto fragments, dataset->GetFragments());
+    for (const auto &frag : fragments)
+    {
+        std::cout << "Frag: " << (*frag)->ToString() << '\n';
+        std::cout << "Partition expression: " << (*frag)->partition_expression().ToString() << '\n';
+    }
+    ARROW_ASSIGN_OR_RAISE(const auto scan_builder, dataset->NewScan());
+    ARROW_ASSIGN_OR_RAISE(const auto scanner, scan_builder->Finish());
+    ARROW_ASSIGN_OR_RAISE(const auto table, scanner->ToTable());
+
+    std::cout << table->Slice(0, 10)->ToString() << std::endl;
+
+    // ARROW_ASSIGN_OR_RAISE(auto tmin, ReadFile(TMIN_PATH));
+    // ARROW_ASSIGN_OR_RAISE(auto tmax, ReadFile(TMAX_PATH));
+    // std::cout << tmin->Slice(0, 10)->ToString() << std::endl;
     return arrow::Status::OK();
 }
 
