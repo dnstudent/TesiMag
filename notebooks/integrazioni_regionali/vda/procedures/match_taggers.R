89830b224b619917fb3bdfcb60a6cc13e4313cfa
dnstudent <davide.nicoli1@studenti.unimi.it>
2023-12-20 19:05:58 +0100

    Merged VDA
    Former-commit-id: 06d8d2d903d160590f1a0b31d997b351b62970ca

--- notebooks/integrazioni_regionali/vda/procedures/match_taggers.R
+++ notebooks/integrazioni_regionali/vda/procedures/match_taggers.R
@@ -2,17 +2,48 @@ library(dplyr, warn.conflicts = FALSE)
 
 
 tag_same_station <- function(analysis) {
-    analysis |> mutate(
-        same_station = (
-            (valid_days_inters > 360) & ((!is.na(strSym) & (f0 > 0.101 | (strSym > 0.93) | distance < 400)) | (is.na(strSym) & (f0 > 0.6)))
-        )
-    )
+    analysis |>
+        mutate(
+            same_station = (
+                (valid_days_inters > 360) & ((!is.na(strSym) & (f0 > 0.101 | (strSym > 0.93) | distance < 400)) | (is.na(strSym) & ((f0 > 0.6) | (distance < 600))))
+            )
+        ) |>
+        group_by(station_id.x, station_id.y) |>
+        mutate(
+            same_station = any(same_station)
+        ) |>
+        ungroup()
 }
 
 tag_unusable <- function(analysis) {
     analysis |> mutate(
         unusable = (
-            !is.na(delT) & delT > 0.7
+            !is.na(monthlydelT) & (abs(monthlydelT) > 0.7 | maeT > 1.5)
+        )
+    )
+}
+
+tag_same_station_internal <- function(analysis) {
+    analysis |>
+        mutate(
+            same_station = (
+                ((valid_days_inters > 360) &
+                    ((!is.na(strSym) &
+                        (f0 > 0.1 | distance < 400)))) |
+                    (is.na(strSym) & (distance < 600))
+            )
+        ) |>
+        group_by(station_id.x, station_id.y) |>
+        mutate(
+            same_station = any(same_station)
+        ) |>
+        ungroup()
+}
+
+tag_unusable_internal <- function(analysis) {
+    analysis |> mutate(
+        unusable = (
+            !is.na(monthlydelT) & (abs(monthlydelT) > 0.7 | maeT > 1.5)
         )
     )
 }
