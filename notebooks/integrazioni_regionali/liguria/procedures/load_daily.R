dca8c6c53d484e6456dc44a932d531b68c5d0f21
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-01-01 17:21:14 +0100

    Ported ARPAL dataset to new db procedure
    Former-commit-id: 3046810101e31a2bc0c43f282f9ac87aa6a2cad1

--- notebooks/integrazioni_regionali/liguria/procedures/load_daily.R
+++ notebooks/integrazioni_regionali/liguria/procedures/load_daily.R
@@ -3,26 +3,36 @@ library(arrow, warn.conflicts = FALSE)
 
 source("src/paths/paths.R")
 
+dataset_spec <- function() {
+    list(
+        "https://ambientepub.regione.liguria.it/SiraQualMeteo/script/PubAccessoDatiMeteo.asp",
+        "regional",
+        "Dataset di ARPA Liguria. Misure giornaliere aggregate su orari UTC (?)"
+    )
+}
+
 load_meta <- function() {
     ds_id <- "ARPAL"
     read_feather(file.path(path.ds, "ARPA", "LIGURIA", "metadata.arrow"), as_data_frame = FALSE) |>
-        mutate(network = ds_id, dataset_id = ds_id, state = "Liguria", across(c(anagrafica, identifier, province, BACINO), ~ cast(., utf8()))) |>
-        rename(station_name = anagrafica, original_id = identifier) |>
+        mutate(network = ds_id, dataset = ds_id, state = "Liguria", across(c(anagrafica, identifier, province, BACINO), ~ cast(., utf8()))) |>
+        rename(name = anagrafica, id = identifier) |>
         collect() |>
-        name_stations() |>
         as_arrow_table()
 }
 
-load_data <- function() {
-    data <- read_parquet(file.path(path.ds, "ARPA", "LIGURIA", "dataset.parquet"), as_data_frame = FALSE) |>
-        mutate(merged = FALSE, across(c(variable, identifier), ~ cast(., utf8())))
+load_data <- function(first_date, last_date) {
+    read_parquet(file.path(path.ds, "ARPA", "LIGURIA", "dataset.parquet"), as_data_frame = FALSE) |>
+        filter(first_date <= date & date <= last_date) |>
+        mutate(dataset = "ARPAL", across(c(variable, identifier), ~ cast(., utf8()))) |>
+        rename(station_id = identifier) |>
+        compute()
 }
 
-load_daily_data.arpal <- function() {
+load_daily_data.arpal <- function(first_date, last_date) {
     meta <- load_meta()
-    data <- load_data() |>
-        left_join(meta |> select(original_id, station_id), join_by(identifier == original_id)) |>
-        as_arrow_table2(data_schema)
+    data <- load_data(first_date, last_date)
+
+    meta <- semi_join(meta, data, join_by(dataset, id == station_id)) |> compute()
 
     list("meta" = meta, "data" = data)
 }
