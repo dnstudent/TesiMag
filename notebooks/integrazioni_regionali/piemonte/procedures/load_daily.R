092429f6a48fe175385e52dfe3a6ae6c292d3f2c
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-01-01 17:50:09 +0100

    Ported Piemonte dataset to new db format

--- notebooks/integrazioni_regionali/piemonte/procedures/load_daily.R
+++ notebooks/integrazioni_regionali/piemonte/procedures/load_daily.R
@@ -4,6 +4,14 @@ library(arrow, warn.conflicts = FALSE)
 source("src/paths/paths.R")
 source("src/database/tools.R")
 
+dataset_spec <- function() {
+    list(
+        "https://utility.arpa.piemonte.it/meteoidro/",
+        "regional",
+        "Dataset di ARPA Piemonte. Dati forniti con aggregazione giornaliera. Accesso tramite API."
+    )
+}
+
 load_meta <- function() {
     col_names <- c(
         "data_inizio_sensore",
@@ -39,30 +47,32 @@ load_meta <- function() {
         group_by(id_punto_misura) |>
         slice_tail() |> # There are duplicate ids; geolocations are identical
         ungroup() |>
-        rename(original_id = id_punto_misura, elevation = quota_stazione, lon = longitudine_e_wgs84_d, lat = latitudine_n_wgs84_d, station_name = denominazione) |>
-        mutate(dataset_id = "ARPAPiemonte", network = "ARPAPiemonte", state = "Piemonte") |>
-        name_stations() |>
-        assertr::assert(assertr::is_uniq, station_id) |>
+        rename(id = id_punto_misura, elevation = quota_stazione, lon = longitudine_e_wgs84_d, lat = latitudine_n_wgs84_d, name = denominazione) |>
+        mutate(dataset = "ARPAPiemonte", network = "ARPAPiemonte", state = "Piemonte") |>
+        assertr::assert(assertr::is_uniq, id) |>
         as_arrow_table()
 }
 
-load_work_data <- function() {
+load_work_data <- function(first_date, last_date) {
     ds_path <- file.path(path.ds, "ARPA", "PIEMONTE", "site-data")
     open_dataset(ds_path) |>
+        filter(first_date <= data & data <= last_date) |>
         select(data, tmax, tmin, id_punto) |>
         mutate(id_punto = cast(id_punto, utf8())) |>
-        rename(original_id = id_punto, T_MIN = tmin, T_MAX = tmax, date = data)
+        rename(station_id = id_punto, T_MIN = tmin, T_MAX = tmax, date = data)
 }
 
-load_daily_data.arpapiemonte <- function() {
-    meta <- load_meta() |> as_arrow_table2(station_schema)
-    data <- load_work_data() |>
-        left_join(meta |> select(original_id, station_id), by = "original_id") |>
-        select(-original_id) |>
-        collect() |>
+load_daily_data.arpapiemonte <- function(first_date, last_date) {
+    meta <- load_meta()
+    data <- load_work_data(first_date, last_date) |>
+        to_duckdb() |>
         pivot_longer(cols = c(T_MIN, T_MAX), names_to = "variable", values_to = "value") |>
-        mutate(merged = FALSE) |>
-        as_arrow_table2(data_schema)
+        mutate(dataset = "ARPAPiemonte") |>
+        filter(!is.na(value)) |>
+        to_arrow() |>
+        compute()
+
+    meta <- semi_join(meta, data, join_by(dataset, id == station_id)) |> compute()
 
     list("meta" = meta, "data" = data)
 }
