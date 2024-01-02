add0d880ef3ed3c333838f16e80ffc05a9b5836e
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-01-02 11:29:43 +0100

    Ported Lombardia dataset to new db approach
    Former-commit-id: 4d9fd0342edcbb3982bbd1a3e7a9b6b8edb18177

--- notebooks/integrazioni_regionali/lombardia/procedures/load_daily.R
+++ notebooks/integrazioni_regionali/lombardia/procedures/load_daily.R
@@ -1,37 +1,47 @@
 library(arrow, warn.conflicts = FALSE)
 library(dplyr, warn.conflicts = FALSE)
 library(tidyr, warn.conflicts = FALSE)
+library(duckdb, warn.conflicts = FALSE)
+library(DBI, warn.conflicts = FALSE)
 
 source("src/database/tools.R")
 source("src/paths/paths.R")
 source("src/analysis/data/quality_check.R")
 
+dataset_spec <- function() {
+    list(
+        "https://www.dati.lombardia.it/Ambiente/Dati-sensori-meteo-2020/erjn-istm/about_data",
+        "regional",
+        "Dataset di ARPA Lombardia. Dati suborari. Aggregazione effettuata manualmente. Dati misurati fino all''orario indicato, ora solare."
+    )
+}
+
 path.lom <- file.path(path.ds, "ARPA", "LOMBARDIA")
 
-load_work_metadata.arpal <- function() {
+load_work_metadata.arpalombardia <- function() {
     path.md <- file.path(path.lom, "Stazioni_Meteorologiche.tsv")
     read_tsv_arrow(path.md,
         col_types = schema(
-            IdSensore = uint32(),
+            IdSensore = int32(),
             IdStazione = utf8(),
             NomeStazione = utf8(),
             Tipologia = utf8(),
             Provincia = utf8(),
             DataStart = timestamp(unit = "s"),
             DataStop = timestamp(unit = "s"),
-            Quota = double(),
-            lng = double(),
-            lat = double()
+            Quota = float64(),
+            lng = float64(),
+            lat = float64()
         ),
         timestamp_parsers = "%d/%m/%Y",
         as_data_frame = FALSE,
         col_select = c("IdSensore", "IdStazione", "NomeStazione", "Tipologia", "Provincia", "DataStart", "DataStop", "Quota", "lng", "lat")
     ) |>
         filter(Tipologia == "Temperatura") |>
-        mutate(across(starts_with("Data"), ~ cast(., date32())), network = "ARPALombardia", state = "Lombardia", dataset_id = "ARPALombardia") |>
+        mutate(across(starts_with("Data"), ~ cast(., date32())), network = "ARPALombardia", state = "Lombardia", dataset = "ARPALombardia") |>
         rename(
-            original_id = IdSensore,
-            station_name = NomeStazione,
+            id = IdSensore,
+            name = NomeStazione,
             province = Provincia,
             elevation = Quota,
             lon = lng,
@@ -39,19 +49,20 @@ load_work_metadata.arpal <- function() {
             original_station_id = IdStazione,
             first_date = DataStart,
             last_date = DataStop
-        )
+        ) |>
+        compute()
 }
 
-open_orig_ds.arpal <- function(metadata) {
+open_orig_ds.arpalombardia <- function(metadata) {
     path.fragments <- file.path(path.lom, "fragments")
 
     read_conf <- list(
-        "schema" = input_csv_schema <- schema(
-            IdSensore = uint32(),
+        "schema" = schema(
+            IdSensore = int32(),
             Data = timestamp(unit = "s"),
             Valore = float64(),
             Stato = utf8(),
-            idOperatore = uint32()
+            idOperatore = int32()
         ),
         "ts_parsers" = csv_timestamp_parsers <- c("%d/%m/%Y %H:%M:%S", "%d/%m/%Y %H:%M:%S.000"),
         "nas" = na_strings <- c("", "NA", "9999.0", "-9999.0", "-9999", "NV", "NC", "ND")
@@ -65,42 +76,42 @@ open_orig_ds.arpal <- function(metadata) {
         col_names = TRUE,
         skip = 1
     ) |>
-        rename(original_id = IdSensore, time = Data, value = Valore) |>
-        filter(as.Date("2000-01-01") <= as.Date(time) & as.Date(time) <= as.Date("2022-12-31")) |>
-        semi_join(metadata, by = "original_id") |>
+        rename(station_id = IdSensore, time = Data, value = Valore) |>
+        semi_join(metadata, join_by(station_id == id)) |>
         # write_dataset(file.path(path.lom, "cache"))
         # open_dataset(file.path(path.lom, "cache")) |>
         mutate(time = time - as.difftime(10 * 60, unit = "secs"))
 }
 
-load_daily_data.arpal <- function() {
-    work_meta <- load_work_metadata.arpal()
-    work_data <- open_orig_ds.arpal(work_meta) |>
+load_work_data.arpalombardia <- function(first_date, last_date, metadata) {
+    conn <- dbConnect(duckdb(), file.path(path.lom, "lombardia.db"), read_only = TRUE)
+    metadata <- metadata |> to_duckdb(con = conn, table_name = "metadata_tmp", auto_disconnect = FALSE)
+    data <- tbl(conn, "full_tmp") |>
+        semi_join(metadata, join_by(station_id == id)) |>
         gross_errors_check(value, thresh = 50) |>
-        group_by(original_id, date = date(time)) |>
+        group_by(station_id, date = as.Date(time)) |>
         summarise(
             T_MIN = min(value, na.rm = TRUE),
             T_MAX = max(value, na.rm = TRUE),
-            any_gross = any(qc_gross, na.rm = TRUE),
+            all_good = all(qc_gross, na.rm = TRUE),
             .groups = "drop"
         ) |>
-        filter(!any_gross) |>
-        select(-any_gross) |>
-        mutate(original_id = cast(original_id, utf8())) |>
-        collect() |>
+        filter(all_good, first_date <= date & date <= last_date) |>
+        select(-all_good) |>
         pivot_longer(cols = c("T_MIN", "T_MAX"), names_to = "variable", values_to = "value") |>
-        mutate(merged = FALSE) |>
-        as_arrow_table()
+        mutate(dataset = "ARPALombardia") |>
+        filter(!is.na(value)) |>
+        to_arrow() |>
+        compute()
+    data
+}
+
+load_daily_data.arpalombardia <- function(first_date, last_date) {
+    work_meta <- load_work_metadata.arpalombardia()
+    work_data <- load_work_data.arpalombardia(first_date, last_date, work_meta)
 
     work_meta <- work_meta |>
-        collect() |>
-        mutate(original_id = as.character(original_id)) |>
-        name_stations() |>
-        as_arrow_table()
-
-    work_data <- work_data |>
-        left_join(work_meta |> select(original_id, station_id), by = "original_id") |>
-        select(-original_id) |>
+        semi_join(work_data, join_by(dataset, id == station_id)) |>
         compute()
 
     list(
