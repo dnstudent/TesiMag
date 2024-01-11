b049bbdb9d821f3350cabb366375884e4e49658f
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-01-11 15:35:50 +0100

    Spring cleanup
    Former-commit-id: 586af720751ab1be54d27e255a1b352ef274b312

--- src/merging/combining/same.R
+++ src/merging/combining/same.R
@@ -5,6 +5,7 @@ library(zeallot, warn.conflicts = FALSE)
 library(igraph, warn.conflicts = FALSE)
 
 source("src/database/write.R")
+source("src/database/query/pairing.R")
 
 annual_index <- function(date) {
     2 * pi * yday(date) / yday(ceiling_date(date, unit = "year") - as.difftime(1, units = "days"))
@@ -178,8 +179,8 @@ concat_merged_and_unmerged <- function(merged_data, whole_database, approved_mat
 }
 
 vertices_groups <- function(graph) {
-    mmb <- components(g, mode = "weak")$membership
-    tibble(id = names(mmb), gid = unname(mmb))
+    mmb <- components(graph, mode = "weak")$membership
+    tibble(id = as.integer(names(mmb)), gid = as.integer(unname(mmb)))
 }
 
 graph_from_isedge <- function(matches, is_edge_variable) {
@@ -191,16 +192,28 @@ graph_from_isedge <- function(matches, is_edge_variable) {
         graph_from_edgelist(directed = FALSE)
 }
 
-group_series_matches <- function(series_matches) {
+add_nonmatching <- function(graph, metadata) {
+    already_there <- V(graph) |>
+        names() |>
+        as.integer()
+
+    missing <- metadata |>
+        distinct(id) |>
+        anti_join(tibble(id = already_there), by = "id")
+
+    add_vertices(graph, nrow(missing), name = missing$id)
+}
+
+group_series_matches <- function(series_matches, metadata) {
     if (distinct(series_matches, variable) |> nrow() > 1L) {
-        stop("The series matches must all be for the same variable")
+        stop("More than one variable detected. This is not supported")
     }
     edges <- series_matches |>
         select(id_x, id_y, tag_same_station, tag_mergeable) |>
         mutate(edge_same_station = tag_same_station, edge_mergeable = tag_same_station & tag_mergeable, .keep = "unused")
 
-    same_station_graph <- graph_using_edge(edges, edge_same_station)
-    merge_graph <- graph_using_edge(edges, edge_mergeable)
+    same_station_graph <- graph_using_edge(edges, edge_same_station) |> add_nonmatching(metadata)
+    merge_graph <- graph_from_isedge(edges, edge_mergeable) |> add_nonmatching(metadata)
 
     list(
         "same_station" = list(
@@ -212,4 +225,68 @@ group_series_matches <- function(series_matches) {
             "vertex_group" = vertices_groups(merge_graph)
         )
     )
+}
+
+set_dataset_priority <- function(metadata, dataset_priority) {
+    metadata |> mutate(dataset = factor(dataset, levels = dataset_priority, ordered = TRUE))
+}
+
+diff_coeffs <- function(monthlydelT, t) {
+    n_data <- length(monthlydelT |> na.omit())
+    coeffs <- if (n_data >= 8) {
+        coeffs <- lm(monthlydelT ~ sin(t / 2) + sin(t) + sin(3 * t / 2))$coefficients
+        names(coeffs) <- c("k0", "k1", "k2", "k3")
+        coeffs
+    } else if (n_data > 2) {
+        c("k0" = mean(monthlydelT, na.rm = TRUE), "k1" = 0, "k2" = 0, "k3" = 0)
+    } else {
+        c("k0" = 0, "k1" = 0, "k2" = 0, "k3" = 0)
+    }
+    as_tibble_row(coeffs)
+}
+
+compute_corrections <- function(lagged_series_matches, data) {
+    monthly_corrections <- pair_full_series(data, lagged_series_matches) |>
+        mutate(delT = value_y - value_x) |>
+        group_by(variable, id_x, id_y, month = month(date), year = year(date)) |>
+        summarise(ymonthlydelT = mean(delT, na.rm = TRUE), .groups = "drop_last") |>
+        summarise(monthlydelT = mean(ymonthlydelT, na.rm = TRUE), .groups = "drop") |>
+        mutate(t = 2 * pi * (month - 0.5) / 12) |>
+        collect()
+
+    monthly_corrections |>
+        group_by(variable, id_x, id_y) |>
+        summarise(coeffs = diff_coeffs(monthlydelT, t), .groups = "drop") |>
+        unnest(coeffs)
+}
+
+recomp_matches <- function(ranked_series_matches) {
+
+}
+
+#' Merges the series in a given group to produce a single series.
+#' ... specifies the order in which the series are merged comparing series metadata: the first series is the one with the highest priority, and so on.
+merge_series_group <- function(series_groups, metadata, data, dataset_priority, ...) {
+    series_groups <- copy_to(metadata$src$con, series_groups, overwrite = TRUE, name = "series_group_tmp")
+    ranked_series_matches <- series_groups |>
+        left_join(metadata, by = "id") |>
+        collect() |>
+        set_dataset_priority(dataset_priority) |>
+        arrange(...) |>
+        group_by(gid) |>
+        mutate(priority = -row_number()) |>
+        ungroup() |>
+        select(gid, variable, id, priority)
+
+    metadata$src$con |> dbExecute("DROP TABLE series_group_tmp")
+
+    ranked_series_matches <- copy_to(data$src$con, ranked_series_matches, overwrite = TRUE, name = "arranged_series_matches_tmp")
+    data |>
+        inner_join(ranked_series_matches, by = c("variable", "station_id" = "id")) |>
+        group_by(variable, gid, date) |>
+        arrange(desc(priority)) |>
+        summarise(id = first(station_id), value = first(value), .groups = "drop")
+    # mutate(station_id = as.character(station_id)) |>
+    # pivot_wider(id_cols = date, names_from = station_id, values_from = value) |>
+    # arrange(date)
 }
