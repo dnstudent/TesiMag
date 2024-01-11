f2a56fee161891a55220a4f5235d22a2df31f02c
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-01-11 15:05:33 +0100

    fixup! Working on station matching
    Former-commit-id: 049fbb1a038d588bcee4b33d57c240112b09dff1

--- notebooks/integrazioni_regionali/toscana/procedures/taggers.R
+++ notebooks/integrazioni_regionali/toscana/procedures/taggers.R
@@ -5,14 +5,41 @@ tag_analysis <- function(analysis) {
         mutate(
             tag_same_station = (
                 (valid_days_inters > 360 &
-                    (f0 > 0.2 & ((dataset_y != "ISAC" & monthlymaeT <= 0.5) | (dataset_y == "ISAC" & monthlymaetT <= 0.8))))
+                    ((dataset_y != "ISAC" & dataset_x != "ISAC") &
+                        ((f0 > 0.2 & monthlymaeT <= 0.5) | fsameint > 0.66 | strSym >= 0.9)
+                    ) |
+                    ((dataset_x == "ISAC" | dataset_y == "ISAC") &
+                        ((f0 > 0.15) | (strSym > 0.9 & abs(delZm) < 100 & monthlymaeT < 1))
+                    )
+                ) |
+                    (valid_days_inters <= 360 &
+                        f0 > 0.97 |
+                        ((dataset_y != "ISAC" & dataset_x != "ISAC") &
+                            distance < 200 | strSym >= 0.93
+                        ) |
+                        ((dataset_x == "ISAC" | dataset_y == "ISAC") &
+
+
+                        )
+                    )
             ),
             tag_mergeable = (
                 (valid_days_inters > 360 &
-                    (f0 > 0.1057 & monthlymaeT < 0.5))
-            )
+                    ((dataset_y != "ISAC" & dataset_x != "ISAC") &
+                        ((f0 > 0.2 | fsameint > 0.66 | strSym >= 0.9) & monthlymaeT <= 0.5)
+                    ) |
+                    ((dataset_x == "ISAC" | dataset_y == "ISAC") &
+                        ((f0 > 0.15) | (strSym > 0.9 & abs(delZm) < 100 & monthlymaeT <= 1))
+                    )
+                ) |
+                    (valid_days_inters <= 360 &
+                        ((dataset_y != "ISAC" & dataset_x != "ISAC") &
+                            distance < 200 | strSym >= 0.93
+                        ) & climaticmaeT < 1.5
+                    )
+            ),
         ) |>
-        group_by(station_id_x, station_id_y) |>
+        group_by(id_x, id_y) |>
         mutate(
             tag_same_station = all(tag_same_station)
         ) |>
