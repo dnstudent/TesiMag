74cbb1ebd8a27f53996c020a39f068dd33e1a2e7
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-01-18 16:27:14 +0100

    Updated ARPAM dataset
    Former-commit-id: bb343a4137cffe031d0c6466d980d3078bae69b5

--- notebooks/integrazioni_regionali/marche/procedures/load_daily.R
+++ notebooks/integrazioni_regionali/marche/procedures/load_daily.R
@@ -11,7 +11,7 @@ dataset_spec <- function() {
     list(
         "http://app.protezionecivile.marche.it/sol/indexjs.sol?lang=it",
         "regional",
-        "Dataset SIRMIP di ARPA Marche"
+        "Dataset SIRMIP di ARPA Marche. Dati a passo giornaliero non validati."
     )
 }
 
@@ -25,45 +25,35 @@ from_degrees <- function(value_str) {
 
 load_meta <- function() {
     arpam_path <- file.path(path.ds, "ARPA", "MARCHE")
-    meta <- vroom::vroom(file.path(arpam_path, "metadata.csv"), trim_ws = TRUE, delim = ",", col_types = "iccdic", show_col_types = FALSE) |>
+    meta <- vroom::vroom(file.path(arpam_path, "metadata.csv"), trim_ws = TRUE, delim = ",", col_types = "iccdicc", show_col_types = FALSE) |>
         as_tibble()
     meta |>
-        rename(
-            lon = Longitudine,
-            lat = Latitudine,
-            elevation = `Quota [m]`,
-            id = `Codice sensore`,
-            name = `Nome stazione`,
-        ) |>
         mutate(
             across(c(lon, lat), ~ str_replace(., "Ḟ", "°")),
             across(c(lon, lat), from_degrees),
-            dataset = "ARPAM",
+            original_dataset = "ARPAM",
             network = "SIRMIP",
-            state = "Marche",
+            state = "Marche"
         ) |>
         as_arrow_table()
 }
 
-load_data <- function(first_date, last_date) {
+load_data <- function() {
     arpam_path <- file.path(path.ds, "ARPA", "MARCHE")
     data <- open_dataset(file.path(arpam_path, "dataset"))
     data |>
         rename(station_id = codice_sensore, T_MIN = tmin, T_MAX = tmax) |>
         filter(quality > 90) |>
         select(!c(quality, num_valori, codice_stazione)) |>
-        mutate(dataset = "ARPAM") |>
         to_duckdb() |>
         pivot_longer(c(T_MIN, T_MAX), names_to = "variable", values_to = "value") |>
-        filter(first_date <= date & date <= last_date) |>
         to_arrow() |>
-        as_arrow_table2(data_schema)
+        mutate(dataset = "ARPAM") |>
+        as_arrow_table()
 }
 
-load_daily_data.arpam <- function(first_date, last_date) {
-    data <- load_data(first_date, last_date)
-    meta <- load_meta() |>
-        semi_join(data, join_by(dataset, id == station_id)) |>
-        compute()
+load_daily_data.arpam <- function() {
+    data <- load_data()
+    meta <- load_meta()
     list("meta" = meta, "data" = data)
 }
