72f87628f8c99dd8fe492c68e6c6380ba81ae20d
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-01-18 18:06:33 +0100

    Updated ARPAUmbria dataset

--- notebooks/integrazioni_regionali/umbria/procedures/load_daily.R
+++ notebooks/integrazioni_regionali/umbria/procedures/load_daily.R
@@ -7,8 +7,6 @@ library(tidyr, warn.conflicts = FALSE)
 
 source("src/paths/paths.R")
 source("src/load/tools.R")
-source("src/analysis/metadata.R")
-source("src/database/definitions.R")
 source("src/database/tools.R")
 
 dataset_spec <- function() {
@@ -19,8 +17,24 @@ dataset_spec <- function() {
     )
 }
 
+load_meta <- function() {
+    dem <- read_stars(file.path("temp", "dem", "dem30.tif"))
+    vroom::vroom(file.path(path.ds, "ARPA", "UMBRIA", "elenco_stazioni.csv"), show_col_types = FALSE, col_types = "iicddccc") |>
+        as_tibble() |>
+        select(-`_id`) |>
+        rename(original_id = ID_STAZIONE, name = NOME_STAZIONE, lat = LAT, lon = LON) |>
+        mutate(
+            state = "Umbria",
+            original_dataset = "ARPAUmbria",
+            network = "ARPAUmbria",
+            kind = "unknown"
+        ) |>
+        mutate(elevation = st_extract(dem, pick(lon, lat) |> as.matrix()) |> pull(1)) |>
+        as_arrow_table()
+}
+
 load_daily_data.umbria <- function(first_date, last_date) {
-    full_table <- read_csv_arrow(
+    data <- read_csv_arrow(
         file.path(path.ds, "ARPA", "UMBRIA", "rilevazioni_temperatura_aria_storico_dati_giornalieri.csv"),
         skip = 1L,
         schema = schema(
@@ -41,36 +55,19 @@ load_daily_data.umbria <- function(first_date, last_date) {
             MINDAY = float64(),
             MAXDAY = float64()
         ),
+        col_select = c("ID_STAZIONE", "ANNO", "MESE", "GIORNO", "MINDAY", "MAXDAY"),
         as_data_frame = FALSE
     ) |>
-        mutate(date = make_date(ANNO, MESE, GIORNO)) |>
-        rename(station_id = ID_STAZIONE, name = NOME_STAZIONE, lat = LATITUDINE, lon = LONGITUDINE, T_MIN = MINDAY, T_MAX = MAXDAY) |>
-        select(!c(ANNO, MESE, GIORNO, AVGDAY)) |>
-        filter(first_date <= date & date <= last_date)
-
-    meta <- full_table |>
-        select(!c(T_MIN, T_MAX, date)) |>
-        distinct() |>
-        mutate(
-            state = "Umbria",
-            dataset = "ARPAUmbria",
-            network = "ARPAUmbria",
-        ) |>
-        rename(id = station_id) |>
-        collect() |>
-        st_md_to_sf(remove = FALSE) |>
-        add_dem_elevations(dem = read_stars(file.path("temp", "dem", "dem30.tif"))) |>
-        st_drop_geometry() |>
-        rename(elevation = dem)
-
-    data <- full_table |>
-        select(station_id, date, T_MIN, T_MAX) |>
+        mutate(date = make_date(ANNO, MESE, GIORNO), .keep = "unused") |>
+        rename(station_id = ID_STAZIONE, T_MIN = MINDAY, T_MAX = MAXDAY) |>
         to_duckdb() |>
         pivot_longer(c(T_MIN, T_MAX), names_to = "variable", values_to = "value") |>
         filter(!is.na(value)) |>
         mutate(dataset = "ARPAUmbria") |>
         to_arrow() |>
-        as_arrow_table2(data_schema)
+        compute()
+
+    meta <- load_meta()
 
     list("meta" = meta, "data" = data)
 }
