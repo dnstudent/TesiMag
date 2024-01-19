e28ee55d808ea61e63026ba6db1b7687050afcbc
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-01-19 17:16:31 +0100

    Updated ARPAE dataset

--- notebooks/integrazioni_regionali/emilia-romagna/procedures/load_daily.R
+++ notebooks/integrazioni_regionali/emilia-romagna/procedures/load_daily.R
@@ -9,43 +9,44 @@ dataset_spec <- function() {
     list(
         "https://dati.datamb.it/dataset/dati-dalle-stazioni-meteo-locali-della-rete-idrometeorologica-regionale/resource/d990dd69-61c1-41d2-802b-86dc293660a8",
         "regional",
-        "Dataset di ARPAE. Passo suborario. Sono stati utilizzati i dati aggregati qualora presenti, altrimenti sono stati aggregati a mano."
+        "Dataset di ARPAE. Portata istantanea. Sono stati utilizzati i dati aggregati qualora presenti, altrimenti sono stati aggregati a mano. L''orario è GMT, a differenza di altre regioni."
     )
 }
 
-load_daily_data.arpae <- function(first_date, last_date) {
-    ds <- open_dataset(file.path(path.ds, "ARPA", "EMILIA-ROMAGNA", "tables"), format = "feather") |>
-        mutate(station_name = cast(name, utf8()), network = cast(network, utf8()), date = date - as.difftime(agg_period, unit = "secs")) |>
+load_daily_data.arpae <- function() {
+    ds <- open_dataset(file.path(path.ds, "ARPA", "EMILIA-ROMAGNA", "OpenData", "tables")) |>
+        # time is the start of the period in GMT
+        mutate(station_name = cast(name, utf8()), network = cast(network, utf8()), time = (force_tz(date, "GMT") - as.difftime(agg_period, unit = "secs"))) |>
+        select(-date) |>
         select(!c(P1, starts_with("level"), name)) |>
-        mutate(original_id = str_c(network, station_name, elevation, sep = "/"))
+        mutate(original_id = str_c("-", str_c(lon, lat, sep = ","), network, sep = "/"))
 
     md <- distinct(ds, original_id, station_name, network, lon, lat, elevation, WMO_block, WMO_num) |>
         mutate(lon = lon / 100000, lat = lat / 100000) |>
-        collect() |>
-        mutate(state = "Emilia-Romagna", dataset = "ARPAE") |>
-        rename(id = original_id, name = station_name) |>
-        assert(is_uniq, id) |>
-        as_arrow_table()
+        mutate(state = "Emilia-Romagna", original_dataset = "ARPAE", kind = "automatica") |>
+        rename(name = station_name) |>
+        compute()
 
     data <- ds |>
-        select(station_id = original_id, date, T, variable, agg_period) |>
-        mutate(T = T - 273.15) |>
-        gross_errors_check(T) |>
-        group_by(station_id, date = as.Date(date)) |>
-        summarise(T_MIN = min(T, na.rm = TRUE), T_MAX = max(T, na.rm = TRUE), qc_gross = any(qc_gross)) |>
-        ungroup() |>
-        filter(qc_gross, first_date <= date & date <= last_date) |>
-        arrange(station_id, date) |>
+        select(station_id = original_id, time, T, variable, agg_period) |>
+        mutate(value = T - 273.15, .keep = "unused") |>
+        qc_gross() |>
+        filter(qc_gross) |>
+        select(-qc_gross) |>
+        mutate(date = as.Date(time, tz = "GMT")) |>
         to_duckdb() |>
+        group_by(station_id, date, agg_period) |>
+        summarise(T_MIN = min(value, na.rm = TRUE), T_MAX = max(value, na.rm = TRUE), .groups = "drop_last") |>
+        # Trusting goes: daily aggregates > hourly aggregates > instantaneous. This is because daily aggregates are computed on GMT day instead of CET, apparently.
+        slice_max(agg_period) |>
+        ungroup() |>
         pivot_longer(c(T_MIN, T_MAX), names_to = "variable", values_to = "value") |>
-        arrange(station_id, variable, date) |>
-        select(!starts_with("qc_")) |>
-        mutate(dataset = "ARPAE") |>
         to_arrow() |>
+        mutate(dataset = "ARPAE") |>
         compute()
 
     md <- md |>
-        semi_join(data, join_by(dataset, id == station_id)) |>
+        semi_join(data, join_by(original_id == station_id)) |>
         compute()
 
     list("meta" = md, "data" = data)
