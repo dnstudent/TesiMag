bfc70d1be463c3118cb864ea328502e1ab7881c8
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-01-23 12:38:45 +0100

    Updated ISAC

--- notebooks/integrazioni_nazionali/ISAC/procedures/load_daily.R
+++ notebooks/integrazioni_nazionali/ISAC/procedures/load_daily.R
@@ -6,30 +6,30 @@ dataset_spec <- function() {
     list(
         "locale",
         "national",
-        "Dataset DPC+? fornito da Michele Brunetti. Utilizzo la versione qc con era5. Probabilmente per varie regioni registra gli estremi delle medie orarie, invece che gli estremi giornalieri."
+        "Dataset DPC+ISAC fornito da Michele Brunetti. Utilizzo la versione qc con era5. Probabilmente per varie regioni registra gli estremi delle medie orarie, invece che gli estremi giornalieri."
     )
 }
 
 load_work_metadata <- function() {
-    tmin <- read.BRUN.metadata("T_MIN", "qc_era5") |>
-        mutate(identifier = as.character(identifier), id = str_replace(identifier, regex("^(TMND_)|(TN_)"), ""))
-    tmax <- read.BRUN.metadata("T_MAX", "qc_era5") |>
-        mutate(identifier = as.character(identifier), id = str_replace(identifier, regex("^(TMXD_)|(TX_)"), ""))
+    tmin <- read.BRUN.metadata("T_MIN", "raw") |>
+        mutate(identifier = as.character(identifier), original_id = str_replace(identifier, regex("^(TMND_)|(TN_)"), ""))
+    tmax <- read.BRUN.metadata("T_MAX", "raw") |>
+        mutate(identifier = as.character(identifier), original_id = str_replace(identifier, regex("^(TMXD_)|(TX_)"), ""))
 
     bind_rows(
         tmin,
         tmax
     ) |>
-        filter(lat > 42) |>
-        mutate(across(c(region_, country, province), as.character), dataset = "ISAC", network = "ISAC") |>
+        # filter(lat > 42) |>
+        mutate(across(c(region_, country, province), as.character), original_dataset = "ISAC", network = if_else(!is.na(internal_id), "DPC", "ISAC"), kind = "unknown") |>
         rename(name = anagrafica, actual_original_id = identifier)
 }
 
-load_data <- function(meta, first_date, last_date) {
-    tmin <- read.BRUN.series("T_MIN", "qc_era5") |>
+load_data <- function(meta) {
+    tmin <- read.BRUN.series("T_MIN", "raw") |>
         rename(value = T_MIN)
 
-    tmax <- read.BRUN.series("T_MAX", "qc_era5") |>
+    tmax <- read.BRUN.series("T_MAX", "raw") |>
         rename(value = T_MAX)
 
     bind_rows(
@@ -42,26 +42,23 @@ load_data <- function(meta, first_date, last_date) {
         #     write_dataset("db.old/tmp/ISAC")
         # open_dataset("db.old/tmp/ISAC") |>
         rename(actual_original_id = identifier) |>
-        filter(first_date <= date & date <= last_date) |>
         mutate(actual_original_id = cast(actual_original_id, utf8()), dataset = "ISAC") |>
         inner_join(
-            meta |> select(actual_original_id, id) |> as_arrow_table(),
+            meta |> select(actual_original_id, original_id) |> as_arrow_table(),
             by = "actual_original_id",
             relationship = "many-to-one"
         ) |>
-        rename(station_id = id) |>
+        rename(station_id = original_id) |>
         as_arrow_table()
 }
 
-load_daily_data.isac <- function(first_date, last_date) {
+load_daily_data.isac <- function() {
     meta <- load_work_metadata()
-    data <- load_data(meta, first_date, last_date)
+    data <- load_data(meta)
 
     meta <- meta |>
-        distinct(dataset, id, .keep_all = TRUE) |>
-        as_arrow_table() |>
-        semi_join(data, join_by(dataset, id == station_id)) |>
-        compute()
+        distinct(original_dataset, original_id, .keep_all = TRUE) |>
+        as_arrow_table()
 
     list("meta" = meta, "data" = data)
 }
