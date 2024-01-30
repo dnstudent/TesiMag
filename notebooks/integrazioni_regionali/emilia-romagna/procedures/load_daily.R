1597f08be62d396f7683befaf7bb53d8e435180d
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-01-30 16:41:47 +0100

    Update ARPAE
    Former-commit-id: 799d39d4946046a5d25b90eaebe39a0402d1de96

--- notebooks/integrazioni_regionali/emilia-romagna/procedures/load_daily.R
+++ notebooks/integrazioni_regionali/emilia-romagna/procedures/load_daily.R
@@ -16,19 +16,13 @@ dataset_spec <- function() {
 load_daily_data.arpae <- function() {
     ds <- open_dataset(file.path(path.ds, "ARPA", "EMILIA-ROMAGNA", "OpenData", "tables")) |>
         # time is the start of the period in GMT
-        mutate(station_name = cast(name, utf8()), network = cast(network, utf8()), time = (force_tz(date, "GMT") - as.difftime(agg_period, unit = "secs"))) |>
+        mutate(name = cast(name, utf8()), network = cast(network, utf8()), time = (force_tz(date, "GMT") - as.difftime(agg_period, unit = "secs"))) |>
         select(-date) |>
-        select(!c(P1, starts_with("level"), name)) |>
-        mutate(original_id = str_c("-", str_c(lon, lat, sep = ","), network, sep = "/"))
-
-    md <- distinct(ds, original_id, station_name, network, lon, lat, elevation, WMO_block, WMO_num) |>
-        mutate(lon = lon / 100000, lat = lat / 100000) |>
-        mutate(state = "Emilia-Romagna", original_dataset = "ARPAE", kind = "automatica") |>
-        rename(name = station_name) |>
-        compute()
+        select(!c(P1, starts_with("level"))) |>
+        mutate(station_id = str_c("-", str_c(lon, lat, sep = ","), network, sep = "/"), series_id = str_c(lon, lat, sep = ","))
 
     data <- ds |>
-        select(station_id = original_id, time, T, variable, agg_period) |>
+        select(station_id, time, T, variable, agg_period) |>
         mutate(value = T - 273.15, .keep = "unused") |>
         qc_gross() |>
         filter(qc_gross) |>
@@ -45,9 +39,30 @@ load_daily_data.arpae <- function() {
         mutate(dataset = "ARPAE") |>
         compute()
 
-    md <- md |>
-        semi_join(data, join_by(original_id == station_id)) |>
+    dates_meta <- data |>
+        group_by(dataset, station_id) |>
+        summarise(station_first = min(date), station_last = max(date), .groups = "drop")
+
+    md <- distinct(ds, station_id, series_id, name, network, lon, lat, elevation, WMO_block, WMO_num) |>
+        mutate(lon = lon / 100000, lat = lat / 100000) |>
+        mutate(dataset = "ARPAE", kind = "automatica") |>
+        mutate(
+            sensor_id = NA_character_,
+            series_first = as.Date(NA_integer_),
+            series_last = as.Date(NA_integer_),
+            town = NA_character_,
+            user_code = NA_character_
+        ) |>
+        left_join(dates_meta, by = c("dataset", "station_id")) |>
+        mutate(
+            sensor_first = station_first,
+            sensor_last = station_last
+        ) |>
         compute()
+
+    # md <- md |>
+    #     semi_join(data, join_by(station_id)) |>
+    #     compute()
 
     list("meta" = md, "data" = data)
 }
