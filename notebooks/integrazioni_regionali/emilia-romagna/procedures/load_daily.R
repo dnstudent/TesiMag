0dbe55147c27808b2cd1b7f41dd385d64d7d9ae2
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-02-05 15:44:55 +0100

    fixup! Update ARPAE
    Former-commit-id: 42b02c12adad51483a808924307cbf34d16c7927

--- notebooks/integrazioni_regionali/emilia-romagna/procedures/load_daily.R
+++ notebooks/integrazioni_regionali/emilia-romagna/procedures/load_daily.R
@@ -13,6 +13,26 @@ dataset_spec <- function() {
     )
 }
 
+load_data <- function() {
+    ds <- open_dataset(file.path(path.ds, "ARPA", "EMILIA-ROMAGNA", "OpenData", "tables")) |>
+        # time is the start of the period in GMT
+        mutate(name = cast(name, utf8()), network = cast(network, utf8()), time = (force_tz(date, "GMT") - as.difftime(agg_period, unit = "secs"))) |>
+        select(-date) |>
+        select(!c(P1, starts_with("level"))) |>
+        mutate(station_id = str_c("-", str_c(lon, lat, sep = ","), network, sep = "/"), series_id = str_c(lon, lat, sep = ",")) |>
+        select(station_id, time, T, variable, agg_period) |>
+        mutate(value = T - 273.15, .keep = "unused") |>
+        mutate(date = as.Date(time + as.difftime(3600, units = "secs")), dataset = "ARPAE") |>
+        filter(agg_period != 86400L, !is.na(value))
+    # tmin <- ds |>
+    #     filter(variable == 3L) |>
+    #     select(dataset, station_id, date, value)
+    # tmax <- ds |>
+    #     filter(variable == 2L) |>
+    #     select(dataset, station_id, date, value)
+    concat_tables(tmin |> mutate(variable = "T_MIN") |> compute(), tmax |> mutate(variable = "T_MAX") |> compute())
+}
+
 load_daily_data.arpae <- function() {
     ds <- open_dataset(file.path(path.ds, "ARPA", "EMILIA-ROMAGNA", "OpenData", "tables")) |>
         # time is the start of the period in GMT
@@ -21,42 +41,44 @@ load_daily_data.arpae <- function() {
         select(!c(P1, starts_with("level"))) |>
         mutate(station_id = str_c("-", str_c(lon, lat, sep = ","), network, sep = "/"), series_id = str_c(lon, lat, sep = ","))
 
+    agg_prios <- arrow_table(agg_period = c(86400L, 3600L, 900L, 0L), priority = c(4L, 1L, 3L, 2L), schema = schema(agg_period = int64(), priority = int32()))
+
     data <- ds |>
         select(station_id, time, T, variable, agg_period) |>
         mutate(value = T - 273.15, .keep = "unused") |>
-        qc_gross() |>
-        filter(qc_gross) |>
-        select(-qc_gross) |>
+        filter(!is.na(value)) |>
+        # qc_gross() |>
+        # filter(qc_gross, agg_period != 86400L) |>
+        # select(-qc_gross) |>
         mutate(date = as.Date(time, tz = "GMT")) |>
-        to_duckdb() |>
         group_by(station_id, date, agg_period) |>
         summarise(T_MIN = min(value, na.rm = TRUE), T_MAX = max(value, na.rm = TRUE), .groups = "drop_last") |>
-        # Trusting goes: daily aggregates > hourly aggregates > instantaneous. This is because daily aggregates are computed on GMT day instead of CET, apparently.
-        slice_max(agg_period) |>
+        left_join(agg_prios, by = "agg_period") |>
         ungroup() |>
+        to_duckdb() |>
+        group_by(station_id, date) |>
+        slice_min(priority) |>
+        select(-priority) |>
         pivot_longer(c(T_MIN, T_MAX), names_to = "variable", values_to = "value") |>
         to_arrow() |>
         mutate(dataset = "ARPAE") |>
         compute()
 
-    dates_meta <- data |>
-        group_by(dataset, station_id) |>
-        summarise(station_first = min(date), station_last = max(date), .groups = "drop")
+    # data <- load_data()
 
     md <- distinct(ds, station_id, series_id, name, network, lon, lat, elevation, WMO_block, WMO_num) |>
         mutate(lon = lon / 100000, lat = lat / 100000) |>
         mutate(dataset = "ARPAE", kind = "automatica") |>
         mutate(
             sensor_id = NA_character_,
+            sensor_first = as.Date(NA_integer_),
+            sensor_last = as.Date(NA_integer_),
+            station_first = as.Date(NA_integer_),
+            station_last = as.Date(NA_integer_),
             series_first = as.Date(NA_integer_),
             series_last = as.Date(NA_integer_),
             town = NA_character_,
             user_code = NA_character_
-        ) |>
-        left_join(dates_meta, by = c("dataset", "station_id")) |>
-        mutate(
-            sensor_first = station_first,
-            sensor_last = station_last
         ) |>
         compute()
 
