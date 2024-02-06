2847c464e4a9793a2c412cacfd8de3189499f6b2
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-02-06 16:02:31 +0100

    Updated ARPAUmbria
    Former-commit-id: 45745025b8c6f7768fbc58baa235d907b385a05d

--- notebooks/integrazioni_regionali/umbria/procedures/load_daily.R
+++ notebooks/integrazioni_regionali/umbria/procedures/load_daily.R
@@ -19,22 +19,40 @@ dataset_spec <- function() {
 
 load_meta <- function() {
     dem <- read_stars(file.path("temp", "dem", "dem30.tif"))
-    vroom::vroom(file.path(path.ds, "ARPA", "UMBRIA", "elenco_stazioni.csv"), show_col_types = FALSE, col_types = "iicddccc") |>
+    info <- vroom::vroom(file.path(path.ds, "ARPA", "UMBRIA", "stazioni_info.csv"), show_col_types = FALSE, col_types = "icddccccdc") |>
+        as_tibble() |>
+        select(station_id, elevation)
+    sensors <- vroom::vroom(file.path(path.ds, "ARPA", "UMBRIA", "elenco_sensori.csv"), show_col_types = FALSE, col_types = "iciccci") |>
+        as_tibble() |>
+        rename(station_id = ID_STAZIONE, sensor_id = ID_SENSORE) |>
+        select(-`_id`)
+    vroom::vroom(file.path(path.ds, "ARPA", "UMBRIA", "elenco_stazioni1.csv"), show_col_types = FALSE, col_types = "iccddccc") |>
         as_tibble() |>
         select(-`_id`) |>
-        rename(original_id = ID_STAZIONE, name = NOME_STAZIONE, lat = LAT, lon = LON) |>
+        rename(station_id = ID_STAZIONE, name = NOME_STAZIONE, lat = LAT, lon = LON, town = COMUNE, province_code = PROVINCIA) |>
         mutate(
-            state = "Umbria",
-            original_dataset = "ARPAUmbria",
+            dataset = "ARPAUmbria",
             network = "ARPAUmbria",
-            kind = "unknown"
+            kind = "unknown",
+            user_code = station_id,
         ) |>
-        mutate(elevation = st_extract(dem, pick(lon, lat) |> as.matrix()) |> pull(1)) |>
+        mutate(
+            # elevation = st_extract(dem, pick(lon, lat) |> as.matrix()) |> pull(1),
+            sensor_first = as.Date(NA_integer_),
+            sensor_last = as.Date(NA_integer_),
+            station_first = as.Date(NA_integer_),
+            station_last = as.Date(NA_integer_),
+            series_first = as.Date(NA_integer_),
+            series_last = as.Date(NA_integer_),
+            series_id = station_id
+        ) |>
+        left_join(sensors, by = "station_id") |>
+        left_join(info, by = "station_id") |>
         as_arrow_table()
 }
 
-load_daily_data.umbria <- function(first_date, last_date) {
-    data <- read_csv_arrow(
+load_data <- function() {
+    read_csv_arrow(
         file.path(path.ds, "ARPA", "UMBRIA", "rilevazioni_temperatura_aria_storico_dati_giornalieri.csv"),
         skip = 1L,
         schema = schema(
@@ -43,7 +61,7 @@ load_daily_data.umbria <- function(first_date, last_date) {
             STRUMENTO = utf8(),
             TIPO_STRUMENTO = utf8(),
             UNITA_MISURA = utf8(),
-            ID_STAZIONE = int32(),
+            ID_STAZIONE = utf8(),
             COMUNE = utf8(),
             NOME_STAZIONE = utf8(),
             LATITUDINE = float64(),
@@ -55,19 +73,30 @@ load_daily_data.umbria <- function(first_date, last_date) {
             MINDAY = float64(),
             MAXDAY = float64()
         ),
-        col_select = c("ID_STAZIONE", "ANNO", "MESE", "GIORNO", "MINDAY", "MAXDAY"),
+        col_select = c("ID_STAZIONE", "ID_SENSORE_DETTAGLIO", "ANNO", "MESE", "GIORNO", "MINDAY", "MAXDAY"),
         as_data_frame = FALSE
     ) |>
         mutate(date = make_date(ANNO, MESE, GIORNO), .keep = "unused") |>
-        rename(station_id = ID_STAZIONE, T_MIN = MINDAY, T_MAX = MAXDAY) |>
+        rename(
+            station_id = ID_STAZIONE,
+            sensor_id = ID_SENSORE_DETTAGLIO,
+            T_MIN = MINDAY,
+            T_MAX = MAXDAY
+        ) |>
         to_duckdb() |>
         pivot_longer(c(T_MIN, T_MAX), names_to = "variable", values_to = "value") |>
         filter(!is.na(value)) |>
         mutate(dataset = "ARPAUmbria") |>
         to_arrow() |>
         compute()
+}
 
-    meta <- load_meta()
+load_daily_data.umbria <- function(first_date, last_date) {
+    data <- load_data()
+    meta <- load_meta() |>
+        semi_join(data, by = c("station_id", "sensor_id")) |>
+        compute()
+    data <- data |> select(-station_id)
 
     list("meta" = meta, "data" = data)
 }
