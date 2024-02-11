3f251eef6060d513d412957034c4f774e5dd6201
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-02-11 21:51:56 +0100

    Working on ARPAFVG

--- notebooks/integrazioni_regionali/FVG/procedures/match_taggers.R
+++ notebooks/integrazioni_regionali/FVG/procedures/match_taggers.R
@@ -1,46 +1,36 @@
 library(dplyr, warn.conflicts = FALSE)
 
-tag_same_station <- function(analysis) {
+tag_same_series <- function(analysis) {
     analysis |>
-        group_by(station_id.x, station_id.y) |>
         mutate(
-            same_station =
-                ((network.y == "Sinottica" & (station_name.x == "" | is.na(station_name.x))) | (network.y != "Sinottica" & station_name.x != "" & !is.na(station_name.x))) &
-                    ((distance < 200) |
-                        (is.na(f0) & distance < 1000) |
-                        (is.na(station_name.x) & (network.y == "Sinottica") & (distance < 1000)) |
-                        (fsameint > 0.62 & all(maeT < 1)) |
-                        (f0 > 0.09 & all(maeT < 1)) |
-                        ((strSym > 0.75 & fsameint > 0.2 & abs(delT) < 0.8)) & all(maeT < 1)),
+            tag_same_sensor = (
+                (overlap_union > 0.9 & f0 > 0.8)
+            ),
+            tag_extavgs = (
+                dataset_x == "ARPAFVG" & valid_days_inters > 160L & abs(balance) > 0.8 & !is.na(strSym) & strSym > 0.8
+            ),
+            tag_dist = (
+                (valid_days_inters < 160 & distance < 600) | (valid_days_inters >= 160 & monthlymaeT < 0.6 & distance < 250)
+            ),
+            tag_ava = (dataset_x == "ARPAFVG" & dataset_y == "ARPAFVG") & (
+                distance < 50
+            ),
+            tag_avs = (dataset_x == "ARPAFVG" & dataset_y == "SCIA") & (
+                distance <= 626L
+            ),
+            tag_avi = (dataset_x == "ARPAFVG" & dataset_y == "ISAC") & (
+                network_y == "DPC" & distance < 1000 & abs(delH) < 100 & monthlymaeT < 0.8
+            ),
+            tag_ivs = (dataset_x == "ISAC" & dataset_y == "SCIA") & (
+                (valid_days_inters >= 160L & f0 > 0.1) | (valid_days_inters < 160L & strSym > )
+            ),
+            tag_suff = (!is.na(strSym) & strSym > 0.999)
         ) |>
-        replace_na(list(same_station = FALSE))
-}
-
-tag_unusable <- function(analysis) {
-    analysis |> mutate(
-        unusable = !is.na(monthlydelT) & (abs(monthlydelT) > 0.75)
-    )
-}
-
-tag_same_station_arpa_merged <- function(analysis) {
-    analysis |>
-        group_by(station_id.x, station_id.y) |>
+        group_by(key_x, key_y) |>
         mutate(
-            same_station = !is.na(f0) & !is.na(station_name.y) & !(network.y == "Sinottica") & (
-                (f0 > 0.1 & all(maeT < 1)) |
-                    (fsameint > 0.571 & all(maeT < 1)) |
-                    (!is.na(strSym) & strSym >= 0.78) |
-                    (distance < 1000) |
-                    (distance < 2200 & delH < 300)
-            )
+            tag_extavgs = all(tag_extavgs) & (prod(balance) < 0),
+            tag_ivs = any(tag_ivs),
+            tag_same_series = tag_suff | tag_extavgs | tag_ava | tag_dist | tag_avs | tag_avi | tag_ivs
         ) |>
         ungroup()
-}
-
-tag_unusable_arpa_merged <- function(analysis) {
-    analysis |> mutate(
-        unusable = (
-            is.na(monthlydelT) | abs(monthlydelT) >= 0.8
-        )
-    )
 }
