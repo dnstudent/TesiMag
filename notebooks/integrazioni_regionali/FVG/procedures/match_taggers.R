58019eb09f922bd84ee501c1ac6b8d0c4d75bcca
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-02-12 11:44:09 +0100

    Tagged FVG

--- notebooks/integrazioni_regionali/FVG/procedures/match_taggers.R
+++ notebooks/integrazioni_regionali/FVG/procedures/match_taggers.R
@@ -3,9 +3,6 @@ library(dplyr, warn.conflicts = FALSE)
 tag_same_series <- function(analysis) {
     analysis |>
         mutate(
-            tag_same_sensor = (
-                (overlap_union > 0.9 & f0 > 0.8)
-            ),
             tag_extavgs = (
                 dataset_x == "ARPAFVG" & valid_days_inters > 160L & abs(balance) > 0.8 & !is.na(strSym) & strSym > 0.8
             ),
@@ -19,18 +16,25 @@ tag_same_series <- function(analysis) {
                 distance <= 626L
             ),
             tag_avi = (dataset_x == "ARPAFVG" & dataset_y == "ISAC") & (
-                network_y == "DPC" & distance < 1000 & abs(delH) < 100 & monthlymaeT < 0.8
+                network_y == "DPC" & distance < 1000 & (is.na(delH) | abs(delH) < 100) & ((valid_days_inters < 160 & strSym > 0.95) | monthlymaeT < 0.8)
             ),
             tag_ivs = (dataset_x == "ISAC" & dataset_y == "SCIA") & (
-                (valid_days_inters >= 160L & f0 > 0.1) | (valid_days_inters < 160L & strSym > )
+                (valid_days_inters >= 160L & f0 > 0.1) | (valid_days_inters < 160L & (!is.na(strSym) & strSym > 0.9))
             ),
-            tag_suff = (!is.na(strSym) & strSym > 0.999)
+            tag_suff = ((!is.na(strSym) & strSym > 0.999) | distance < 50),
+            tag_mergeable = (valid_days_inters < 160L & climaticmaeT < 1.8) | monthlymaeT < 1
         ) |>
         group_by(key_x, key_y) |>
         mutate(
             tag_extavgs = all(tag_extavgs) & (prod(balance) < 0),
             tag_ivs = any(tag_ivs),
-            tag_same_series = tag_suff | tag_extavgs | tag_ava | tag_dist | tag_avs | tag_avi | tag_ivs
+            tag_same_series = (tag_suff | tag_extavgs | tag_ava | tag_dist | tag_avs | tag_avi | tag_ivs) & (valid_days_inters < 500 | any(monthlymaeT < 1)),
+            tag_same_sensor = (
+                (overlap_union > 0.9 & tag_same_series)
+            ),
+            tag_same_station = (
+                overlap_union > 0.9 & tag_same_series
+            ),
         ) |>
         ungroup()
 }
