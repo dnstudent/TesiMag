676af2b0e6f3ec227b358af43cecc063e67dd546
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-02-14 23:25:34 +0100

    Merged FVG with SCIA and ISAC
    Former-commit-id: 84d7555c6488d17171442ac9f212a68b386a2fd9

--- notebooks/integrazioni_regionali/FVG/procedures/match_taggers.R
+++ notebooks/integrazioni_regionali/FVG/procedures/match_taggers.R
@@ -7,7 +7,7 @@ tag_same_series <- function(analysis) {
                 dataset_x == "ARPAFVG" & valid_days_inters > 160L & abs(balance) > 0.8 & !is.na(strSym) & strSym > 0.8
             ),
             tag_dist = (
-                (valid_days_inters < 160 & distance < 600) | (valid_days_inters >= 160 & monthlymaeT < 0.6 & distance < 250)
+                (valid_days_inters < 160 & distance < 600) | (valid_days_inters >= 160 & monthlymaeT < 0.65 & distance < 250)
             ),
             tag_ava = (dataset_x == "ARPAFVG" & dataset_y == "ARPAFVG") & (
                 distance < 50
@@ -21,7 +21,7 @@ tag_same_series <- function(analysis) {
             tag_ivs = (dataset_x == "ISAC" & dataset_y == "SCIA") & (
                 (valid_days_inters >= 160L & f0 > 0.1) | (valid_days_inters < 160L & (!is.na(strSym) & strSym > 0.9))
             ),
-            tag_suff = ((!is.na(strSym) & strSym > 0.999) | distance < 50),
+            tag_suff = ((!is.na(strSym) & strSym > 0.999) | distance < 50 | (valid_days_inters > 160L & f0 > 0.3)),
             tag_mergeable = (valid_days_inters < 160L & climaticmaeT < 1.8) | monthlymaeT < 1
         ) |>
         group_by(key_x, key_y) |>
@@ -37,4 +37,45 @@ tag_same_series <- function(analysis) {
             ),
         ) |>
         ungroup()
+}
+
+tag_manual <- function(tagged_analysis) {
+    tagged_analysis |>
+        mutate(
+            tag_same_series =
+            # (dataset_x == "ARPAFVG" & dataset_y == "ISAC" & name_x == "Gorgo" & name_y == "GORGO") |
+                (tag_same_series &
+                    !(dataset_x == "ARPAFVG" & dataset_y == "ISAC" &
+                        (
+                            (sensor_key_x == 37L & sensor_key_y == 2059L) |
+                                (sensor_key_x == 25L & sensor_key_y == 1336L) |
+                                (sensor_key_x == 13L & sensor_key_y == 797L) |
+                                (sensor_key_x == 31L & sensor_key_y == 1607L) |
+                                (sensor_key_x == 50L & sensor_key_y == 3185L) |
+                                (sensor_key_x == 22L & sensor_key_y == 1148L) |
+                                (sensor_key_x == 52L & sensor_key_y == 3311L)
+                        )
+                    ) &
+                    !(dataset_x == "ISAC" & dataset_y == "SCIA" &
+                        (
+                            (sensor_key_x == 2059L & sensor_key_y == 1978L) |
+                                # (sensor_key_x == 1257L & sensor_key_y == 38L) |
+                                (sensor_key_x == 3185L & sensor_key_y == 3116L) |
+                                (sensor_key_x == 3185L & sensor_key_y == 3059L) |
+                                (sensor_key_x == 1148L & sensor_key_y == 1159L) |
+                                (sensor_key_x == 3130L & sensor_key_y == 1911L)
+                        )
+                    ) &
+                    !(dataset_x == "ARPAFVG" & dataset_y == "SCIA" &
+                        (
+                            (sensor_key_x == 13L & sensor_key_y == 576L) |
+                                (sensor_key_x == 31L & sensor_key_y == 1468L)
+                        )
+                    ) &
+                    !(dataset_x == "ARPAFVG" & name_x == "Piancavallo") &
+                    !((dataset_x == "SCIA" & sensor_key_x == 3059L) | (dataset_y == "SCIA" & sensor_key_y == 3059L)) &
+                    !(dataset_x == "ISAC" & dataset_y == "SCIA" & sensor_key_x == 924L & sensor_key_y == 623L) &
+                    !(dataset_x == "ISAC" & sensor_key_x == 1257L & !(sensor_key_y == 38L & dataset_y == "SCIA")) &
+                    !(dataset_y == "ISAC" & sensor_key_y == 1257L))
+        )
 }
