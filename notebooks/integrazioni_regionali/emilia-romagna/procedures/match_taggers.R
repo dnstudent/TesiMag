142a1e754717fa6d3669633926323052453c2cd6
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-02-16 11:11:55 +0100

    Merged Dext3r into ER
    Former-commit-id: 67b2e864d82a67ae148b5d930531a3dfb76fca6c

--- notebooks/integrazioni_regionali/emilia-romagna/procedures/match_taggers.R
+++ notebooks/integrazioni_regionali/emilia-romagna/procedures/match_taggers.R
@@ -2,28 +2,32 @@
 # - SCIA-SCIA:
 #   - noto serie aggregate secondo differenti definizioni di giorno: es LACOSTAMAIATICO vs Maiatico
 #   - locali, agrmet, spdstra sembrano aggregati sulla data GMT
+# - Dext3r comprende tutte le serie ARPAE, però sono meno aggiornate. Vanno matchate SOLAMENTE su station_id_arpae vs series_id_dext3r
 
 tag_same_series <- function(analysis) {
     analysis |>
         mutate(
+            # Same series
+            tag_sseries_dvd = (dataset_x == "Dext3r" & dataset_y == "Dext3r") &
+                (
+                    (distance <= 200 & ((valid_days_inters < 160L & strSym > 0.6) | (valid_days_inters >= 160L & (f0 > 0.1 | (fsameint > 0.28 & monthlymaeT < 0.8))))) |
+                        (distance > 200 & ((valid_days_inters >= 160L & f0 > 0.6) | (valid_days_inters < 160 & strSym > 0.93 & climaticmaeT < 1.5)))
+                ),
+            # Summary
             tag_same_sensor = (overlap_union > 0.9 & f0 > 0.9),
-            tag_same_station =
-            # ARPAE-specific
-                (dataset_x == "ARPAE" & dataset_y == "ARPAE") & ((overlap_union > 0.8 & f0 > 0.9) | (valid_days_inters == 0L & distance < 1)),
-            tag_same_series =
-            # ARPAE-specific
-                ((dataset_x == "ARPAE" & dataset_y == "ARPAE") & (distance < 200) & (valid_days_inters < 10 | f0 > 0.9)) |
-                    # SCIA-specific
-                    ((dataset_x == "SCIA" & dataset_y == "SCIA") & (
-                        (valid_days_inters >= 160L & f0 > 0.2) |
-                            (valid_days_inters >= 30 & f0 >= 0.9 & strSym > 0.6) |
-                            (valid_days_inters < 30L & (distance < 50 | strSym > 0.99))
-                    )) |
-                    # ARPAE -> SCIA
-                    ((dataset_x == "ARPAE" & dataset_y == "SCIA") &
-                        ((valid_days_inters >= 160L & f0 > 0.3) |
-                        
-                        )
-                    )
+            tag_same_station = (overlap_min > 0.9 & f0 > 0.8),
+            tag_same_series = tag_sseries_dvd,
         )
+}
+
+tag_manual <- function(tagged_analysis) {
+    tagged_analysis |> mutate(
+        tag_same_series = tag_same_series &
+            !(sensor_key_x == 1203L & sensor_key_y == 1348L) &
+            !(sensor_key_x == 583L & sensor_key_y == 1180L) &
+            !(sensor_key_x == 583L & sensor_key_y == 650L) &
+            !(sensor_key_x == 975L & sensor_key_y == 1442L) &
+            !(sensor_key_x == 236L & sensor_key_y == 1650L) &
+            !(sensor_key_x == 1440L & sensor_key_y == 1650L)
+    )
 }
--- notebooks/integrazioni_regionali/emilia-romagna/procedures/tagger.R
+++
@@ -1,7 +0,0 @@
-tag_mergeable <- function(analysis) {
-    analysis |>
-        mutate(
-            tag_mergeable =
-                ((valid_days_inters > 350L) & (f0 > 0.2 | distance < 300) & monthlymaeT <= 0.5)
-        )
-}
