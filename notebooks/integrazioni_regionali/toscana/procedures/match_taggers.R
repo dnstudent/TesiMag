a99d97e35997f89a82c4c6dcb1889963de37f7e7
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-03-07 13:51:12 +0100

    fixup! Merged TOS

--- notebooks/integrazioni_regionali/toscana/procedures/match_taggers.R
+++ notebooks/integrazioni_regionali/toscana/procedures/match_taggers.R
@@ -5,11 +5,15 @@ tag_same_series <- function(analysis) {
         mutate(
             tag_same_sensor = (overlap_union > 0.9 & f0 > 0.9),
             tag_same_station = (overlap_union > 0.8 & f0 > 0.8),
-            tag_same_series = distance < 300 & !(name_x == "Vallombrosa" & name_y == "Vallombrosa"),
+            tag_same_series = !(name_x == "Vallombrosa" & name_y == "Vallombrosa") &
+                (distance < 500 | (strSym == 1 & valid_days_inters == 0L & distance < 1000)),
             tag_mergeable = TRUE,
         )
 }
 
 tag_manual <- function(tagged_analysis) {
-    tagged_analysis
+    tagged_analysis |>
+        mutate(
+            tag_same_series = tag_same_series & !(sensor_key_x == 160L & sensor_key_y == 369L),
+        )
 }
