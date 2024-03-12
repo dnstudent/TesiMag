65e0c69dbcff84956e176ba5cc66dc6163d98afa
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-03-12 19:29:56 +0100

    Fixing QC to keep integer series
    Former-commit-id: f1c2889359f312bd8aa45cf2408ab2625fdd394e

--- src/analysis/data/quality_check.R
+++ src/analysis/data/quality_check.R
@@ -5,61 +5,55 @@ qc_excursion <- function(data, min_excursion = 0, max_excursion = 50) {
     data |>
         pivot_wider(names_from = variable, values_from = value) |>
         rename(T_MIN = `-1`, T_MAX = `1`) |>
-        mutate(excursion = abs(T_MAX - T_MIN)) |>
-        mutate(qc_excursion = is.na(excursion) | (0 < excursion & excursion < 50)) |>
+        mutate(excursion = T_MAX - T_MIN) |>
+        mutate(qc_excursion = is.na(excursion) | (min_excursion <= excursion & excursion <= max_excursion)) |>
         select(-excursion) |>
         pivot_longer(cols = c("T_MIN", "T_MAX"), names_to = "variable", values_to = "value") |>
         mutate(variable = case_match(variable, "T_MIN" ~ -1L, "T_MAX" ~ 1L)) |>
         filter(!is.na(value))
 }
 
-qc_gross <- function(data, threshold = 50) {
+qc_gross <- function(data, min_T = -30, max_T = 50) {
     data |>
-        mutate(qc_gross = is.na(value) | (abs(value) <= threshold))
+        mutate(qc_gross = is.na(value) | (min_T <= value & value <= max_T))
 }
 
-flag_integers <- function(data) {
+flag_integers <- function(data, int_threshold = 1e-4) {
     data |>
-        mutate(is_integer = abs(trunc(value) - value) < 1e-4)
+        mutate(is_integer = abs(trunc(value) - value) < int_threshold)
 }
 
-assign_repetition_gids <- function(data) {
+assign_repetition_gids <- function(data, int_threshold = 1e-4) {
     data |>
-        flag_integers() |>
         group_by(variable, sensor_key) |>
         window_order(date) |>
         mutate(
-            prev_is_different = (abs(value - lag(value)) > 1e-4) |> as.integer() |> coalesce(1L),
-            not_consecutive_ints = (!is_integer | !lag(is_integer)) |> as.integer() |> coalesce(1L),
+            prev_is_different = (abs(value - lag(value)) > int_threshold) |> as.integer() |> coalesce(1L),
         ) |>
         mutate(
             consecutive_val_gid = cumsum(prev_is_different),
-            consecutive_int_gid = cumsum(not_consecutive_ints)
         ) |>
         ungroup() |>
-        select(!c(is_integer, prev_is_different, not_consecutive_ints))
+        select(!c(prev_is_different))
 }
 
-qc_repetitions <- function(data, threshold = 8L) {
+qc_repetitions <- function(data, consecutive_int_threshold = 8L, consecutive_float_threshold = 4L, is_int_threshold = 1e-4) {
     data |>
-        assign_repetition_gids() |>
+        assign_repetition_gids(is_int_threshold) |>
+        flag_integers() |>
         group_by(variable, sensor_key, consecutive_val_gid) |>
         mutate(
-            qc_repeated_values = n() < threshold
+            # qc_repeated_values = n() < threshold
+            qc_repeated_values = if_else(all(is_integer, na.rm = TRUE), n() < consecutive_int_threshold, n() < consecutive_float_threshold)
         ) |>
         ungroup() |>
-        group_by(variable, sensor_key, consecutive_int_gid) |>
-        mutate(
-            qc_repeated_integers = n() < threshold
-        ) |>
-        ungroup() |>
-        select(!c(consecutive_val_gid, consecutive_int_gid))
+        select(!c(consecutive_val_gid, is_integer))
 }
 
-qc1 <- function(raw_data_tbl) {
+qc1 <- function(raw_data_tbl, min_excursion = 0, max_excursion = 50, min_T = -30, max_T = 50, consecutive_int_threshold = 8L, consecutive_float_threshold = 4L, is_int_threshold = 1e-4) {
     raw_data_tbl |>
-        qc_excursion() |>
-        qc_gross() |>
-        qc_repetitions() |>
-        mutate(valid = qc_gross & qc_excursion & qc_repeated_values & qc_repeated_integers)
+        qc_excursion(min_excursion, max_excursion) |>
+        qc_gross(min_T, max_T) |>
+        qc_repetitions(consecutive_int_threshold, consecutive_float_threshold, is_int_threshold) |>
+        mutate(valid = qc_gross & qc_excursion & qc_repeated_values)
 }
