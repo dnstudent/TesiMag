df56761a60aa123965dbfe94a2b53c84ebfd9b30
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-04-09 16:04:41 +0200

    Redid TOS
    Former-commit-id: 9a41f96d75d6d6cf9ee3637133e687cff531ea9f

--- src/database/query/metadata.R
+++ src/database/query/metadata.R
@@ -2,10 +2,20 @@ library(dplyr, warn.conflicts = FALSE)
 library(dbplyr, warn.conflicts = FALSE)
 library(DBI, warn.conflicts = FALSE)
 
-query_metadata <- function(statconn) {
-    tbl(statconn, "raw_station_geo")
-}
+source("src/database/query/spatial.R")
 
-query_duck_metadata <- function(dataconn) {
-    tbl(dataconn, "raw_stations_tmp")
+similar_namesdist <- function(metadata, statconn, threshold = 0.8) {
+    metadata <- select(metadata, name, key, lon, lat)
+    pairs <- cross_join(metadata, metadata, suffix = c("_x", "_y")) |>
+        filter(key_x != key_y) |>
+        mutate(
+            norm_name_x = name_x |> lower() |> replace("_", " ") |> nfc_normalize() |> strip_accents() |> trim(),
+            norm_name_y = name_y |> lower() |> replace("_", " ") |> nfc_normalize() |> strip_accents() |> trim(),
+            strSym = jaro_winkler_similarity(norm_name_y, norm_name_x)
+        ) |>
+        filter(strSym >= threshold) |>
+        collect()
+    query_distance(pairs, statconn) |>
+        collect() |>
+        left_join(pairs |> select(key_x, key_y, strSym), by = c("key_x", "key_y"))
 }
