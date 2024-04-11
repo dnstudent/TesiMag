e94bd1ad615598918707ef330f1d10603f14d40d
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-04-11 12:10:13 +0200

    Redid VEN and UMB
    Former-commit-id: e892a5d85442b2fe075b08b2ad2864431c8eea60

--- notebooks/ds_regionali/umbria/procedures/match_taggers.R
+++ notebooks/ds_regionali/umbria/procedures/match_taggers.R
@@ -4,11 +4,16 @@ tag_same_series <- function(analysis) {
     analysis |> mutate(
         tag_same_sensor = FALSE,
         tag_same_station = FALSE,
-        tag_same_series = (valid_days_inters > 160L & f0 > 0.2),
+        tag_same_series = (valid_days_inters >= 100L & f0 > 0.15) | distance < 10,
         tag_mergeable = TRUE
     )
 }
 
 tag_manual <- function(tagged_analysis) {
-    tagged_analysis
+    tagged_analysis <- tagged_analysis |>
+        mutate(
+            tag_same_series = tag_same_series & !(
+                dataset_x == "ARPAUmbria" & dataset_y == "ARPAUmbria" & sensor_key_x == 46L & sensor_key_y == 47L
+            )
+        )
 }
