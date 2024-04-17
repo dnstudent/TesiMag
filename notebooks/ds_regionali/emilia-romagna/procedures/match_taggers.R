ed864d9e6fe6385182a96a21ebf12dd92da6df34
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-04-17 09:50:26 +0200

    Redid ER
    Former-commit-id: 4c2a8afdb6ff32e8fc5a9b487d402ac74d42f57c

--- notebooks/ds_regionali/emilia-romagna/procedures/match_taggers.R
+++ notebooks/ds_regionali/emilia-romagna/procedures/match_taggers.R
@@ -32,29 +32,41 @@ tag_same_series <- function(analysis) {
 tag_manual <- function(tagged_analysis) {
     tagged_analysis |> mutate(
         tag_same_series = tag_same_series &
-            !(dataset_x == "Dext3r" & dataset_y == "Dext3r" &
-                (
-                    (sensor_key_x == 559L | sensor_key_y == 559L) | # Ferrara AM
-                        (sensor_key_x == 130L & sensor_key_y == 134L) | # Bologna urbana / meteo
-                        (sensor_key_x == 696L & sensor_key_y == 707L) | # Imola / 2
-                        (sensor_key_x == 1294L & sensor_key_y == 1295L) | # Rimini
-                        (sensor_key_y == 73L) # Bardi Centrale
-                )
+            !(
+                (dataset_x == "Dext3r" & dataset_y == "Dext3r" &
+                    (
+                        (sensor_key_x == 559L | sensor_key_y == 559L) | # Ferrara AM
+                            (sensor_key_x == 130L & sensor_key_y == 134L) | # Bologna urbana / meteo
+                            (sensor_key_x == 696L & sensor_key_y == 707L) | # Imola / 2
+                            (sensor_key_x == 1294L & sensor_key_y == 1295L) | # Rimini
+                            (sensor_key_y == 73L) # Bardi Centrale
+                    )) |
+                    (dataset_x == "Dext3r" & dataset_y == "ISAC" &
+                        (
+                            (sensor_key_x == 1060L & sensor_key_y == 2477L) # Parma Uni / Synop
+                        )
+                    )
+
             ) |
             (dataset_x == "Dext3r" & dataset_y == "Dext3r" &
                 (
                     (sensor_key_x == 904L & sensor_key_y == 905L) | # Monte Cimone
-                        (sensor_key_x == 1294L & sensor_key_y == 1304L) # Rimini
+                        (sensor_key_x == 790L & sensor_key_y == 807L) | # Malborghetto
+                        (sensor_key_x == 1294L & sensor_key_y == 1304L) | # Rimini
+                        (sensor_key_x == 466L & sensor_key_y == 468L) | # Copparo
+                        (sensor_key_x == 576L & sensor_key_y == 577L) | # Finale Emilia
+                        (sensor_key_x == 580L & sensor_key_y == 582L) | # Fiorenzuola
+                        (sensor_key_x == 877L & sensor_key_y == 880L) # Modena
                 )
             ) |
             (dataset_x == "SCIA" & dataset_y == "SCIA" &
                 (
-                    sensor_key_x == 2708L & sensor_key_y == 2709L # Parma Synop
+                    (sensor_key_x == 2708L & sensor_key_y == 2709L) # Parma Synop
                 )
             ) |
             (dataset_x == "ISAC" & dataset_y == "SCIA" &
                 (
-                    sensor_key_x == 2477L & sensor_key_y %in% c(2708L, 2709L) # Parma Synop
+                    (sensor_key_x == 2477L & sensor_key_y %in% c(2708L, 2709L)) # Parma Synop
                 )
             )
     )
