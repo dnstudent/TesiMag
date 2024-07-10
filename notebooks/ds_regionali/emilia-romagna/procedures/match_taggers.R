cee1c699a4428ccc6fbc5057a686fb91e9916620
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-07-10 09:04:22 +0200

    Updated ER after db reset
    Former-commit-id: 7b59a05502790ff7a1ba50e6a65278938fd75599

--- notebooks/ds_regionali/emilia-romagna/procedures/match_taggers.R
+++ notebooks/ds_regionali/emilia-romagna/procedures/match_taggers.R
@@ -4,22 +4,22 @@ tag_same_series <- function(analysis) {
     analysis |>
         mutate(
             # Same series
-            tag_sseries_dvd = (dataset_x == "Dext3r" & dataset_y == "Dext3r") &
+            tag_sseries_dvd = !!datasets_are("Dext3r", "Dext3r") &
                 (
                     (distance <= 200 & ((valid_days_inters < 160L & strSym > 0.6) | (valid_days_inters >= 160L & (f0 > 0.1 | (fsameint > 0.28 & monthlymaeT < 0.8))))) |
                         (distance > 200 & ((valid_days_inters >= 160L & f0 > 0.6) | (valid_days_inters < 160 & strSym > 0.93 & climaticmaeT < 1.5))) |
                         (distance < 150) |
                         (strSym > 0.999 & abs(delH) < 50 & distance < 1310)
                 ),
-            tag_sseries_dvs = (dataset_x == "Dext3r" & dataset_y == "SCIA") &
+            tag_sseries_dvs = !!datasets_are("Dext3r", "SCIA") &
                 (
                     !is.na(f0) & f0 > 0.4
                 ),
-            tag_sseries_dvi = (dataset_x == "Dext3r" & dataset_y == "ISAC") &
+            tag_sseries_dvi = !!datasets_are("Dext3r", "ISAC") &
                 (
                     !is.na(f0) & f0 > 0.3
                 ),
-            tag_sseries_ivs = (dataset_x == "ISAC" & dataset_y == "SCIA") &
+            tag_sseries_ivs = !!datasets_are("ISAC", "SCIA") &
                 (
                     !is.na(f0) & f0 > 0.2
                 ),
@@ -83,41 +83,37 @@ tag_manual <- function(tagged_analysis) {
             !(
                 (!!datasets_are("Dext3r", "Dext3r") &
                     (
-                        !!in_sensor_keys(559L) | # Ferrara AM
-                            !!sensor_keys_are(130L, 134L) | # Bologna urbana / meteo
-                            !!sensor_keys_are(696L, 707L) | # Imola / 2
-                            !!sensor_keys_are(1294L, 1295L) | # Rimini
-                            !!sensor_keys_are(1099L, 1100L) | # Pianello Val Tidone
-                            (sensor_key_y == 73L) | # Bardi Centrale
-                            !!sensor_keys_are(995L, 996L) # Novafeltria
+                        !!one_station_is("Dext3r", series_id = "-/1161250,4481556/syrep") | # Ferrara AM
+                            !!one_station_is("Dext3r", series_id = "-/1162101,4483241/idrost") | # Ferrara idrost
+                            !!one_station_is("Dext3r", series_id = "-/1171267,4434581/idrost") | # Imola idrost / recenti
+                            !!series_ids_are_("-/1171267,4434581/idrost", "-/1165247,4437911/profe") | # Imola / 2
+                            !!one_station_is("Dext3r", series_id = "-/1261278,4402444/syrep") | # Rimini Syrep
+                            !!series_ids_are_("-/940179,4495086/idrost", "-/943104,4493712/simnpr") | # Pianello Val Tidone
+                            (series_id_y == "-/970958,4461327/idrost") | # Bardi Centrale
+                            !!series_ids_are_("-/1228611,4390111/simnbo", "-/1229171,4389008/idrost") # Novafeltria
                     )) |
-                    (!!datasets_are_asym("Dext3r", "ISAC") &
+                    (!!datasets_are("Dext3r", "ISAC") &
                         (
-                            !!sensor_keys_are_asym(1060L, 2477L) # Parma Uni / Synop
+                            !!series_ids_are("-/1031495,4480454/idrost", "PAR") # Parma Uni / Synop
                         )
                     )
-
             ) |
-            (dataset_x == "Dext3r" & dataset_y == "Dext3r" &
+            (!!datasets_are("Dext3r", "Dext3r") &
                 (
-                    (sensor_key_x == 904L & sensor_key_y == 905L) | # Monte Cimone
-                        (sensor_key_x == 790L & sensor_key_y == 807L) | # Malborghetto
-                        (sensor_key_x == 1294L & sensor_key_y == 1304L) | # Rimini
-                        (sensor_key_x == 466L & sensor_key_y == 468L) | # Copparo
-                        (sensor_key_x == 576L & sensor_key_y == 577L) | # Finale Emilia
-                        (sensor_key_x == 580L & sensor_key_y == 582L) | # Fiorenzuola
-                        (sensor_key_x == 877L & sensor_key_y == 880L) | # Modena
-                        (sensor_key_x == 596L & sensor_key_y == 1066L) #  Foce / Passo Radici
+                    (
+                        !!series_ids_are_("-/1070000,4419361/syrep", "-/1070000,4418333/idrost") | # Monte Cimone
+                            !!series_ids_are_("-/1093431,4464801/idrost", "-/1091699,4465639/urbane") # Modena / Urbana
+                    )
                 )
             ) |
-            (dataset_x == "SCIA" & dataset_y == "SCIA" &
+            (!!datasets_are("SCIA", "SCIA") &
                 (
-                    (sensor_key_x == 2708L & sensor_key_y == 2709L) # Parma Synop
+                    !!user_codes_are_("161300", "162591") # Parma Synop
                 )
             ) |
-            (dataset_x == "ISAC" & dataset_y == "SCIA" &
+            (!!datasets_are("ISAC", "SCIA") &
                 (
-                    (sensor_key_x == 2477L & sensor_key_y %in% c(2708L, 2709L)) # Parma Synop
+                    !!series_ids_are("PAR", "6829") # Parma Synop
                 )
             )
     )
