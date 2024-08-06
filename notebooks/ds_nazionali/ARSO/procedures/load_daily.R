ba1a096e4b25b6114bc3db43fb0b0ba0876a59d3
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-08-06 22:32:56 +0200

    Draft of ARSO ds

---
+++ notebooks/ds_nazionali/ARSO/procedures/load_daily.R
@@ -0,0 +1,99 @@
+library(arrow, warn.conflicts = F)
+library(dplyr, warn.conflicts = F)
+library(sf, warn.conflicts = F)
+library(vroom, warn.conflicts = F)
+
+source("src/paths/paths.R")
+source("src/load/tools.R")
+
+dataset_spec <- function() {
+    list(
+        "https://meteo.arso.gov.si/met/sl/archive/",
+        "national",
+        "Dataset del ministero dell'Ambiente Sloveno. Varie reti di misura (climatologiche, automatiche, \"principali\")."
+    )
+}
+
+load_meta <- function(ita_bounds) {
+    meta <- vroom(fs::path(path.ds, "meteo.si", "stations.csv"), col_types = cols(
+        station_id = col_integer(),
+        name = col_character(),
+        lon = col_double(),
+        lat = col_double(),
+        elevation = col_double(),
+        type = col_integer(),
+        kind = col_character()
+    ), locale = locale(encoding = "utf-8")) |>
+        as_tibble() |>
+        mutate(
+            dataset = "ARSO",
+            user_code = as.character(station_id),
+            network = kind,
+            sensor_id = NA_character_,
+            series_id = as.character(station_id),
+            sensor_first = as.Date(NA_integer_),
+            sensor_last = as.Date(NA_integer_),
+            station_first = as.Date(NA_integer_),
+            station_last = as.Date(NA_integer_),
+            series_first = as.Date(NA_integer_),
+            series_last = as.Date(NA_integer_),
+            province = NA_character_,
+            town = NA_character_
+        )
+
+    geometa <- meta |>
+        st_md_to_sf()
+    close_stations <- geometa |>
+        st_filter(ita_bounds, .predicate = st_is_within_distance, dist = units::set_units(100, "km"))
+
+    excluded_but_close <- geometa |>
+        anti_join(close_stations |> st_drop_geometry(), by = "station_id") |>
+        st_filter(close_stations, .predicate = st_is_within_distance, dist = units::set_units(500, "m")) |>
+        st_drop_geometry()
+
+    bind_rows(close_stations |> st_drop_geometry(), excluded_but_close)
+}
+
+load_data.trad <- function(meta, dataconn) {
+    open_dataset(fs::path(path.ds, "meteo.si", "trad_ds.arrow"), format = "arrow") |>
+        semi_join(meta |> filter(type != 4L), by = "station_id") |>
+        rename(T_MIN = tmin, T_MAX = tmax) |>
+        mutate(date = as.Date(time)) |>
+        select(-time) |>
+        to_duckdb(con = dataconn) |>
+        pivot_longer(cols = c("T_MIN", "T_MAX"), names_to = "variable", values_to = "value") |>
+        filter(!is.na(value)) |>
+        to_arrow() |>
+        relocate(station_id, date, variable, value) |>
+        compute()
+}
+
+load_data.auto <- function(meta, dataconn) {
+    # ATTENZIONE! CONTROLLARE TMIN <= TMAX, DISPONIBILITA'/GIORNO, PRESENZA DI TUTTE LE MISURE
+    open_dataset(fs::path(path.ds, "meteo.si", "auto_ds.arrow"), format = "arrow") |>
+        semi_join(meta |> filter(type == 4L), by = "station_id") |>
+        to_duckdb(con = dataconn) |>
+        pivot_longer(cols = c("t2mmin", "t2mmax"), names_to = "variable", values_to = "value") |>
+        filter(!is.na(value)) |>
+        group_by(station_id, date = as.Date(time)) |>
+        summarise(T_MIN = min(value, na.rm = T), T_MAX = max(value, na.rm = T), .groups = "drop") |>
+        pivot_longer(cols = c("T_MIN", "T_MAX"), names_to = "variable", values_to = "value") |>
+        to_arrow() |>
+        relocate(station_id, date, variable, value) |>
+        compute()
+}
+
+load_data <- function(meta, dataconn) {
+    concat_tables(
+        load_data.trad(meta, dataconn),
+        load_data.auto(meta, dataconn),
+        unify_schemas = FALSE
+    ) |>
+        mutate(dataset = "ARSO", station_id = as.character(station_id)) |>
+        compute()
+}
+
+load_daily_data.arso <- function(ita_bounds, dataconn) {
+    meta <- load_meta(ita_bounds)
+    list(meta = meta |> mutate(station_id = as.character(station_id)), data = load_data(meta, dataconn))
+}
---
+++ notebooks/ds_nazionali/ARSO/procedures/match_taggers.R
@@ -0,0 +1,31 @@
+source("src/merging/pairing.R")
+
+at_least_3 <- function(x) {
+    expr((abs({{ x }} - round({{ x }}, 3L)) > 1e-4))
+}
+
+are_precise <- function(lon, lat) {
+    expr((!!at_least_3({{ lon }}) & !!at_least_3({{ lat }})))
+}
+
+reliable_distance <- expr((!!are_precise(lon_x, lon_y) & !!are_precise(lat_x, lat_y)))
+
+
+tag_same_series <- function(analysis) {
+    analysis |>
+        mutate(
+            tag_good_diff = valid_days_inters > 100L & (!is.na(f0noint) & f0noint > 0.15) & distance < 2000,
+            tag_proximity = distance < 100 & abs(delH) < 50,
+            # Summary
+            tag_same_sensor = (overlap_union > 0.9 & f0 > 0.9),
+            tag_same_station = (overlap_min > 0.9 & f0 > 0.8),
+            tag_same_series = tag_good_diff | tag_proximity,
+        )
+}
+
+
+tag_manual <- function(tagged_analysis) {
+    tagged_analysis |> mutate(
+        tag_same_series = tag_same_series
+    )
+}
