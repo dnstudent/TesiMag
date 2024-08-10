44d7fc5e14de1b42a65fb65e5ce4bc95daa6247c
dnstudent <davide.nicoli1@studenti.unimi.it>
2024-08-10 08:58:27 +0200

    Manual corrections on SWI

--- notebooks/ds_nazionali/SWISS/procedures/match_taggers.R
+++ notebooks/ds_nazionali/SWISS/procedures/match_taggers.R
@@ -3,10 +3,12 @@ source("src/merging/pairing.R")
 tag_same_series <- function(analysis) {
     analysis |>
         mutate(
+            tag_samecode_swiss = !!datasets_are_("MeteoSwissNBCN", "SwissMetNet") & series_id_x == series_id_y,
+            tag_samecode_isac = dataset_x == "ISAC" & (str_c("0-20000-0-", user_code_x) == str_c(user_code_y, "0")),
             # Summary
             tag_same_sensor = FALSE,
             tag_same_station = FALSE,
-            tag_same_series = distance < 150,
+            tag_same_series = distance < 190 | tag_samecode_swiss | tag_samecode_isac,
         )
 }
 
@@ -14,9 +16,7 @@ tag_same_series <- function(analysis) {
 tag_manual <- function(tagged_analysis) {
     tagged_analysis |> mutate(
         tag_same_series = tag_same_series | (
-            (!!datasets_are_("ISAC", "MeteoSwiss") & (
-                !!series_ids_are_("066304_MG", "0-20000-0-06792") # Samedan
-            ))
+            !!series_ids_are_("066304_MG", "SAM") # Samedan
         )
     )
 }
